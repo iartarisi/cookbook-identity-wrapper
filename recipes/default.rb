fb08fc5e26c3ade806855ad870e0fb728a473499
Ionuț Arțăriși <ionut@artarisi.eu>
2013-09-19 11:43:59 +0200

    rely on openstack-common for database+user creation

--- recipes/default.rb
+++ recipes/default.rb
@@ -21,10 +21,12 @@ class ::Chef::Recipe
   include ::Openstack
 end
 
-node.override[:openstack][:developer_mode] = true
-node.override[:openstack][:db][:service_type] = "postgresql"
-node.override[:openstack][:db][:port] = "5432"
-
+node[:openstack][:db][:identity][:username] = node[:keystone][:db][:user]
+# TODO this used to be:
+# node.set_unless['keystone']['db']['password'] = secure_password
+# Figure out if we want to keep generating passwords manually here, or
+# if we want to use data bags
+node.set[:openstack][:db][:identity][:password] = db_password 'keystone'
 
 #### cargo culting from barclamp-keystone
 env_filter = " AND database_config_environment:database-config-#{node[:keystone][:database_instance]}"
@@ -35,65 +37,31 @@ if sqls.length > 0
 else
     sql = node
 end
-include_recipe "database::client"
-backend_name = Chef::Recipe::Database::Util.get_backend_name(sql)
-include_recipe "#{backend_name}::client"
-include_recipe "#{backend_name}::python-client"
 
-db_provider = Chef::Recipe::Database::Util.get_database_provider(sql)
-db_user_provider = Chef::Recipe::Database::Util.get_user_provider(sql)
-privs = Chef::Recipe::Database::Util.get_default_priviledges(sql)
-url_scheme = backend_name
-
-::Chef::Recipe.send(:include, Opscode::OpenSSL::Password)
-
-# TODO this used to be:
-# node.set_unless['keystone']['db']['password'] = secure_password
-# Figure out if we want to keep generating passwords manually here, or
-# if we want to use data bags
-node.set[:keystone][:db][:password] = db_password 'keystone'
-
+########## end of cargo culting
 
 sql_address = Chef::Recipe::Barclamp::Inventory.get_network_by_type(sql, "admin").address if sql_address.nil?
 Chef::Log.info("Database server found at #{sql_address}")
 
-db_conn = { :host => sql_address,
-            :username => "db_maker",
-            :password => sql["database"][:db_maker_password] }
+# TODO this will normally be set by the (opscode) database::server
+# recipe, but we haven't gotten to using that yet
+node[:postgresql][:password][:postgres] = 'secret'
 
-# Create the Keystone Database
-database "create #{node[:keystone][:db][:database]} database" do
-    connection db_conn
-    database_name node[:keystone][:db][:database]
-    provider db_provider
-    action :create
-end
+# TODO developer mode is set so we don't have to handle data bags yet
+node.set[:openstack][:developer_mode] = true
+node.set[:openstack][:db][:service_type] = "postgresql"
+node.set[:openstack][:db][:port] = "5432"
+node.set[:openstack][:db][:identity][:host] = sql_address
 
-database_user "create keystone database user" do
-    connection db_conn
-    username node[:keystone][:db][:user]
-    password node[:keystone][:db][:password]
-    host '%'
-    provider db_user_provider
-    action :create
-end
+# this used to be hardcoded in database/attributes, now it's hardcoded here
+node.set[:openstack][:db][:identity][:db_type] = "postgresql"
+Chef::Log.error(sql[:database][:sql_engine])
+node.set[:openstack][:db][:identity][:db_name] = node[:keystone][:db][:database]
+node.set[:openstack][:db][:identity][:port] = "5432"
 
-database_user "grant database access for keystone database user" do
-    connection db_conn
-    username node[:keystone][:db][:user]
-    password node[:keystone][:db][:password]
-    database_name node[:keystone][:db][:database]
-    host '%'
-    privileges privs
-    provider db_user_provider
-    action :grant
-end
-########## end of cargo culting
-
-node.override[:openstack][:db][:identity][:host] = sql_address
-node.override[:openstack][:db][:identity][:db_type] = "postgresql"
-node.override[:openstack][:db][:identity][:port] = "5432"
-node.override[:openstack][:db][:identity][:password] = node[:keystone][:db][:password]
+db_create_with_user("identity",
+  node[:openstack][:db][:identity][:username],
+  node[:openstack][:db][:identity][:password])
 
 include_recipe "openstack-identity::server"
 include_recipe "openstack-identity::registration"
